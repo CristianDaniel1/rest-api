754af428865a76bb1f19da45aacc3f40f4543fff
CristianDaniel1 <cristiandaniel.1050@gmail.com>
2024-01-16 19:58:48 -0300

    Commit Inicial

---
+++ schemas/arts.js
@@ -0,0 +1,41 @@
+const zod = require('zod');
+
+const artSchema = zod.object({
+  title: zod.string({
+    invalid_type_error: 'Art title must be a string',
+    required_error: 'Art title is required.',
+  }),
+  artist: zod.string(),
+  date: zod.string(),
+  prompt: zod.string().default('Prompt não informado.'),
+  art: zod.string().url({
+    message: 'Art must be a valid URL',
+  }),
+  categories: zod.array(
+    zod.enum([
+      'Anime',
+      'Personagem',
+      'Comida',
+      'Animais',
+      'Ficção Científica',
+      'Paisagem',
+    ]),
+    {
+      required_error: 'Art category is required.',
+      invalid_type_error: 'Art category must be an array of enum Categories',
+    }
+  ),
+});
+
+function validateArt(input) {
+  return artSchema.safeParse(input);
+}
+
+function validatePartialArt(input) {
+  return artSchema.partial().safeParse(input);
+}
+
+module.exports = {
+  validateArt,
+  validatePartialArt,
+};
